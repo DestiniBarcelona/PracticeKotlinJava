eea566cba54f384fe2522d6d32cb1c2cfabb124d
Destini Barcelona <destinibarcelona@yahoo.com>
2020-09-16 10:47:18 -0400

    First commit

---
+++ src/main/java/com/example/demo/JavaService.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class JavaService {
+
+  public void sayHello() {
+    System.out.println("Java says 'Hello World!'");
+  }
+}
---
+++ src/main/java/com/example/demo/KotlinService.kt
@@ -0,0 +1,7 @@
+package com.example.demo
+
+class KotlinService {
+    fun sayHello() {
+        System.out.println("Kotlin says 'Hello World!'")
+    }
+}
