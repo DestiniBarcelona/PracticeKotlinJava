dcecdb3d4ed96f7d8ba38b40d54b4e2c6db4c5b4
Destini Barcelona <destinibarcelona@yahoo.com>
2020-09-17 09:44:46 -0400

    completed basics, will check to reduce code and add comments for teachability

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import jdk.nashorn.internal.runtime.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @Logger
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
